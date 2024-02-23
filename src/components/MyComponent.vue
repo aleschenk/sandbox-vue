f0af559dc972080886fbb037a9bf475acf177e66
Alejandro Schenk <ale.schenk@gmail.com>
2024-02-23 01:48:06 -0300

    first commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import Rating from 'primevue/rating'
+
+  defineProps<{ msg: string }>()
+
+  const count = ref(0)
+  const value = ref(2)
+</script>
+
+<template>
+  <Rating v-model="value" />
+</template>
+
+<style scoped lang="vueScss">
+
+</style>
